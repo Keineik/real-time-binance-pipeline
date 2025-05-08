6b5d2513d898ef82562d29d3a266eeddfec544c9
Shadow <vile2040@gmail.com>
2025-05-08 21:38:05 +0700

    Fix Load

--- src/Load/22127218.py
+++ src/Load/22127218.py
@@ -2,12 +2,17 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, explode
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType, ArrayType, DoubleType
 
+KAFKA_BROKER = "localhost:9094"
+MONGODB_URI = "mongodb://localhost:27017"
+MONGODB_DATABASE = "crypto"
+
 def main():
     # Khởi tạo Spark session
     spark = SparkSession.builder \
         .appName("LoadStage-ZScoreToMongoDB") \
-        .config("spark.mongodb.output.uri", "mongodb://localhost:27017/crypto") \
+        .config("spark.mongodb.write.connection.uri", MONGODB_URI) \
         .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.cores.max", "3")\
         .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
@@ -26,10 +31,12 @@ def main():
     # Đọc từ Kafka topic 'btc-price-zscore'
     raw_zscore = spark.readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "kafka:9092") \
+        .option("kafka.bootstrap.servers", KAFKA_BROKER) \
         .option("subscribe", "btc-price-zscore") \
         .option("startingOffsets", "latest") \
         .load()
+
+    print("Reading from Kafka topic 'btc-price-zscore'...")
 
     # Phân tích dữ liệu JSON
     parsed_stream = raw_zscore.selectExpr("CAST(value AS STRING) as json") \
@@ -48,21 +55,22 @@ def main():
         .withWatermark("timestamp", "10 seconds")  # Handle late data (up to 10s)
 
     # Ghi từng window vào MongoDB collection riêng biệt
-    window_values = ["30s", "1m", "5m", "15m", "30m", "1h"]
-    queries = []
+    window_values = ["5s", "10s", "20s", "40s"]
     for window in window_values:
+        print(f"Writing to MongoDB collection for window: {window}...")
         filtered = parsed_stream.filter(col("window") == window)
-        query = filtered.writeStream \
+        filtered.writeStream \
             .format("mongodb") \
             .option("checkpointLocation", f"/tmp/checkpoints/mongo-{window}") \
-            .option("collection", f"btc-price-zscore-{window}") \
+            .option("spark.mongodb.connection.uri", MONGODB_URI) \
+            .option("spark.mongodb.database", MONGODB_DATABASE) \
+            .option("spark.mongodb.collection", f"btc-price-zscore-{window}") \
             .outputMode("append") \
             .start()
-        queries.append(query)
 
     # Đợi tất cả truy vấn kết thúc
-    for q in queries:
-        q.awaitTermination()
+    spark.streams.awaitAnyTermination()
+    
 
 if __name__ == "__main__":
     main()
